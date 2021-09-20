0797c25d103768d417f79a1a524001c492538418
DavidAduaka <70069730+DavidAduaka@users.noreply.github.com>
2021-09-19 21:54:25 -0400

    Challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,17 @@
 library(dplyr)
+
+# Deliverable 1
+
 MechaCar_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
 lm(mpg ~  vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_table) #generate multiple linear regression model
 
 summary(lm(mpg ~  vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_table)) #generate summary statistics
 
+#Deliverable 2 
+
+SusCoil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- SusCoil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
+
+lot_summary <- SusCoil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
