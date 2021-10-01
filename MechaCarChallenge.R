f21731166d1ccb49b1514cb5845ca1a0752c2b8e
DavidAduaka <70069730+DavidAduaka@users.noreply.github.com>
2021-09-30 20:02:55 -0400

    Challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,3 +15,13 @@ SusCoil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFact
 total_summary <- SusCoil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
 
 lot_summary <- SusCoil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
+
+#Deliverable 3 
+
+t.test(SusCoil_table$PSI, mu=1500) #compare PSI of lots versus population mean
+
+t.test(SusCoil_table$PSI, mu=1500, subset=(SusCoil_table(Manufacturing_Lot=="Lot1"))) #compare PSI of lot 1 versus population mean
+
+t.test(SusCoil_table$PSI, mu=1500, subset=(SusCoil_table(Manufacturing_Lot=="Lot2"))) #compare PSI of lot 2 versus population mean
+
+t.test(SusCoil_table$PSI, mu=1500, subset=(SusCoil_table(Manufacturing_Lot=="Lot3"))) #compare PSI of lot 3 versus population mean
